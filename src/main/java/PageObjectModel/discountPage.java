df44225f0ffd007486786135d372e3f2f5f3bd5f
TechnoStudy <info@techno.study>
2020-02-04 19:42:51 -0500

    cucumber

--- src/main/java/PageObjectModel/discountPage.java
+++ src/main/java/PageObjectModel/discountPage.java
@@ -38,4 +38,5 @@ public class discountPage extends AbstractClass{
     }
 
 
+
 }
--- src/main/java/stepDefinition/discountSteps.java
+++ src/main/java/stepDefinition/discountSteps.java
@@ -1,11 +1,13 @@
 package stepDefinition;
 
+import PageObjectModel.discountPage;
 import PageObjectModel.leftNav;
 import cucumber.api.java.en.Given;
 
 public class discountSteps {
 
     leftNav lv = new leftNav();
+    discountPage dp = new discountPage();
 
     @Given("^Click on Discount$")
     public void click_on_Discount() throws Throwable {
@@ -13,18 +15,18 @@ public class discountSteps {
     }
 
     @Given("^Type in the Description \"(.*?)\"$")
-    public void type_in_the_Description(String arg1) throws Throwable {
-
+    public void type_in_the_Description(String description) throws Throwable {
+        dp.typeInthedescription( description );
     }
 
     @Given("^Type in the IntegrationCode \"(.*?)\"$")
-    public void type_in_the_IntegrationCode(String arg1) throws Throwable {
-
+    public void type_in_the_IntegrationCode(String intCode) throws Throwable {
+        dp.typeInIntegrationCode( intCode );
     }
 
     @Given("^Type in the priority \"(.*?)\"$")
-    public void type_in_the_priority(String arg1) throws Throwable {
-
+    public void type_in_the_priority(String priority) throws Throwable {
+        dp.typeIninputPriority( priority );
     }
 
 }
