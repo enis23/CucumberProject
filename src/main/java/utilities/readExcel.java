4c300497607c37846e7a019cd73e019927072aef
TechnoStudy <info@techno.study>
2020-02-06 20:17:57 -0500

    cucumber

--- src/main/java/utilities/readExcel.java
+++ src/main/java/utilities/readExcel.java
@@ -1,25 +1,42 @@
 package utilities;
 
+import org.apache.poi.ss.usermodel.*;
+
 import java.io.FileInputStream;
 
 public class readExcel {
 
-        public String getData(String pathOfTheExcel){
+        public String getData(String pathOfTheExcel , String sheetName){
 
             String path = ReadPropertiesFile.getData( pathOfTheExcel );
 
-            FileInputStream inStream =null;
+            FileInputStream inStream = null;
 
-            try{
-                inStream =new FileInputStream( path );
-            }catch(Exception e){
-                System.out.println(e.getMessage());
-            }
+                try{
+                    inStream =new FileInputStream( path );
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+
+            Workbook workbook = null;
+
+                try{
+                    workbook = WorkbookFactory.create( inStream );
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+
+            Sheet sheet = workbook.getSheet( sheetName );
+
+            Row row;
+
+            Cell cell;
+
+            int maxRow = sheet.getPhysicalNumberOfRows();
+
+            System.out.println(maxRow);
 
             
-
-
-
 
             return "a";
         }
