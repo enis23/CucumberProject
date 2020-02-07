312203888c86ae7dcff9d0592cdc17a3b35bb247
TechnoStudy <info@techno.study>
2020-02-06 19:40:36 -0500

    cucumber

--- src/main/java/utilities/myExcel.java
+++ src/main/java/utilities/myExcel.java
@@ -1,5 +1,11 @@
 package utilities;
 
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class myExcel {
 
     /*
@@ -15,9 +21,24 @@ public class myExcel {
             1)Able to get all the data from excel so it is easy maintain
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String path = "src/test/java/excelFiles/releaseNotes.xlsx";
 
+//        if your utilities in the src test java folder
+//        String path2 = "excelFiles/releaseNotes.xlsx";
+
+        FileInputStream inStream = new FileInputStream( path );
+
+        Workbook workbook = WorkbookFactory.create( inStream );
+
+        Sheet sheet = workbook.getSheet( "Sheet1" );
+
+        Row row = sheet.getRow( 0 );
+
+        Cell cell = row.getCell( 0 );
+
+        System.out.println(cell);
 
     }
 
