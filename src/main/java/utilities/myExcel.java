87ad0ea9b6a508d5c3ae78a62cabd2835a3888f1
TechnoStudy <info@techno.study>
2020-02-06 19:22:46 -0500

    cucumber

---
+++ src/main/java/utilities/myExcel.java
@@ -0,0 +1,29 @@
+package utilities;
+
+public class myExcel {
+
+    /*
+        How to read data in the excel
+
+        Use it in the application
+
+        Write in the excel
+     */
+
+    /*
+        Advantage:
+            1)Able to get all the data from excel so it is easy maintain
+     */
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+
+
+
+
+
+}
